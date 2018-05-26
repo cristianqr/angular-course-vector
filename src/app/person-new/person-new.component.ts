2e777bebfe4428986cbe971912b3d496504c82ba
cquispe <cquispe@vectoritcgroup.com>
2018-05-26 13:05:24 -0500

    class05 final

--- src/app/person-new/person-new.component.ts
+++ src/app/person-new/person-new.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {Person} from '../models/person.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-person-new',
@@ -8,12 +9,20 @@ import {Person} from '../models/person.model';
 })
 export class PersonNewComponent implements OnInit {
   @Output() save = new EventEmitter<Person>();
+  @ViewChild('f') form: NgForm;
   constructor() { }
 
   ngOnInit() {
   }
 
-  onSubmit({value}: {value: Person}) {
+  onSubmit({value, invalid}: {value: Person, invalid: boolean}) {
+    Object.keys(this.form.controls).forEach(item => {
+      this.form.controls[item]['touched'] = true;
+    });
+    if (invalid) {
+      console.log('datos invalidos');
+      return;
+    }
     this.save.emit(value);
   }
 
